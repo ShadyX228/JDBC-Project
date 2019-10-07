694400d19e4a63b6aaae6ac0a31f67bfec332f7e
ShadyX228 <44401671+ShadyX228@users.noreply.github.com>
2019-10-07 16:46:14 +0400

    Test2

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,33 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(1);
+    public static void main(String[] args)
+            throws
+            SQLException,
+            NoSuchMethodException,
+            InvocationTargetException,
+            IllegalAccessException,
+            IOException  {
+        Properties properties = new Properties();
+        FileInputStream input = new FileInputStream("connection.properties");
+        properties.load(input);
+
+        Database testDatabase = new Database(
+                properties.getProperty("jdbc.driver"),
+                properties.getProperty("jdbc.url"),
+                properties.getProperty("jdbc.user"),
+                properties.getProperty("jdbc.password"),
+                properties.getProperty("jdbc.dbname")
+        );
+
+        String method;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter method name: ");
+        method = in.next();
+
+        testDatabase.execute(method);
+
+
     }
 }

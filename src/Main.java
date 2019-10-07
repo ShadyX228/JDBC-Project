a2f189ce167c63fea7fe6a85c060bb562b0854eb
ShadyX228 <44401671+ShadyX228@users.noreply.github.com>
2019-10-07 16:33:02 +0400

    Test2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(1);

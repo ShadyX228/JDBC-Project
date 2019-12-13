15b949b4ff2df7de8024a7892eda0e72a7d711ee
ShadyX228 <44401671+ShadyX228@users.noreply.github.com>
2019-12-13 17:00:17 +0400

    Фиксы.

--- src/main/java/servlets/Teacher/TeacherAdder.java
+++ src/main/java/servlets/Teacher/TeacherAdder.java
@@ -1,6 +1,10 @@
 package servlets.Teacher;
 
+import dbmodules.dao.GroupDAO;
+import dbmodules.dao.StudentDAO;
 import dbmodules.dao.TeacherDAO;
+import dbmodules.tables.Group;
+import dbmodules.tables.Student;
 import dbmodules.tables.Teacher;
 import dbmodules.types.Gender;
 
@@ -31,25 +35,26 @@ public class TeacherAdder extends HttpServlet {
                 || birth.isEmpty()
                 || gender.isEmpty()) {
             message += printMessage(2,"Переданы пустые значения.");
+            response.getWriter().write(message);
         } else {
             boolean check = true;
             message += "Значения не пустые " + printMessage(1, "OK.") +
                     "Проверяю корректность введенных параметров...<br>";
-            message += "Имя: " + name +  printMessage(1, "OK.");
+            message += "Имя: " + name + printMessage(1, "OK.");
 
             message += "День рождения: " + birth;
-            LocalDate birthday = LocalDate.of(2000,01,01);
+            LocalDate birthday = LocalDate.of(2000, 01, 01);
             try {
                 birthday = LocalDate.parse(birth);
-                message +=  printMessage(1, "OK.");;
+                message += printMessage(1, "OK.");
             } catch (DateTimeParseException | InputMismatchException e) {
-                message +=  printMessage(2, "Ошибка: некорректный ввод.");;
+                message += printMessage(2, "Ошибка: некорректный ввод.");
                 check = false;
             }
 
             message += "Пол: " + gender;
             Gender genderParsed = checkGender(gender);
-            if(Objects.isNull(genderParsed)) {
+            if (Objects.isNull(genderParsed)) {
                 message += printMessage(2, "Ошибка: некорректный ввод.");
                 check = false;
             } else {
@@ -57,22 +62,22 @@ public class TeacherAdder extends HttpServlet {
             }
 
             message += "Пытаюсь добавить... ";
-            if(!check) {
-                message += printMessage(2,"Ошибка. Одно или несколько полей не прошли проверку.");
+            if (!check) {
+                message += printMessage(2, "Ошибка. Одно или несколько полей не прошли проверку.");
+                response.getWriter().write(message);
             } else {
-                TeacherDAO teacherDAO = new TeacherDAO();
-                teacherDAO.add(new Teacher(
+                Teacher teacher = new Teacher(
                         name,
                         birthday.getYear(),
                         birthday.getMonthValue(),
                         birthday.getDayOfMonth(),
                         genderParsed
-                ));
-                message += printMessage(1,"Запись добавлена.");
+                );
+                TeacherDAO teacherDAO = new TeacherDAO();
+                teacherDAO.add(teacher);
+                teacherDAO.closeEM();
+                response.getWriter().write("Запись <span class=\"lastId\">" + teacher.getId() + "</span> добавлена.");
             }
-
-
         }
-        response.getWriter().write(message);
     }
 }

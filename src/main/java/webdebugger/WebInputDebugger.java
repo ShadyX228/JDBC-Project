8b52f246eca871df83fca45c9b732c7587f0aa02
ShadyX228 <44401671+ShadyX228@users.noreply.github.com>
2020-03-05 17:19:36 +0400

    Cleaning debugger class.

--- src/main/java/webdebugger/WebInputDebugger.java
+++ src/main/java/webdebugger/WebInputDebugger.java
@@ -23,29 +23,6 @@ import java.util.Objects;
 import static dbmodules.types.Criteria.*;
 
 public class WebInputDebugger {
-    public static Criteria checkCriteria(String crit) {
-        switch (crit.toUpperCase()) {
-            case "ID": {
-                return ID;
-            }
-            case "NAME": {
-                return NAME;
-            }
-            case "BIRTH": {
-                return BIRTH;
-            }
-            case "GENDER": {
-                return GENDER;
-            }
-            case "GROUP": {
-                return GROUP;
-            }
-            case "ALL": {
-                return ALL;
-            }
-        }
-        return null;
-    }
     public static Gender checkGender(String genderInput) {
         for (Gender gender : Gender.values()) {
             if (gender.getValue().equals(genderInput)) {
@@ -61,91 +38,5 @@ public class WebInputDebugger {
             e.printStackTrace();
             return null;
         }
-    }
-    public static Group checkGroup
-            (int number, GroupDAO groupDAO) {
-        try {
-            return groupDAO.select(number);
-        } catch (InputMismatchException | IndexOutOfBoundsException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-    public static String parseCriteria
-            (Criteria criteria, String critVal) {
-        switch (criteria) {
-            case ID: {
-                try {
-                    Integer.parseInt(critVal);
-                    return critVal;
-                } catch (InputMismatchException | NumberFormatException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-            case NAME: {
-                return critVal;
-            }
-            case GENDER: {
-                try {
-                    Gender.valueOf(critVal.toUpperCase());
-                    return critVal.toUpperCase();
-                } catch (IllegalArgumentException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-            case GROUP: {
-                GroupDAOimpl groupDAO = new GroupDAOimpl();
-                try {
-                    Group group = groupDAO.select(Integer.parseInt(critVal));
-                    if (!Objects.isNull(group)) {
-                        return Integer.toString(group.getNumber());
-                    } else {
-                        return null;
-                    }
-                } catch (InputMismatchException
-                        | IndexOutOfBoundsException
-                        | NumberFormatException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-            case BIRTH: {
-                try {
-                    LocalDate.parse(critVal);
-                    return critVal;
-                } catch (DateTimeParseException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-            case ALL: {
-                return "";
-            }
-        }
-        return null;
-    }
-
-   public static void setStudentJSONObjectState(List<Student> students,
-                                                Map<Integer, Integer> groups,
-                                                List<String> errors, JSONObject jsonObject) {
-       if(!students.isEmpty()) {
-           jsonObject.accumulate("students", students);
-           for(Student student : students) {
-               groups.put(student.getId(), student.getGroup().getNumber());
-           }
-           jsonObject.accumulate("groups", groups);
-       } else {
-           errors.add("Список пуст.");
-       }
-    }
-
-    public static void setQueryParametres(HttpServletRequest request,
-                                          HttpServletResponse response)
-            throws UnsupportedEncodingException {
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
-        request.setCharacterEncoding("UTF-8");
     }
 }
